a3384bd9763cc29b8b19d3b89ad9da074689c368
NooB-AssassinZ3 <noob.assassinz3orstep@gmail.com>
2016-12-13 16:52:50 -0600

    C#
    added a C# Solution

---
+++ Lite-Sword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lite_Sword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Title = "Lite Sword 0.1.0";
+            Print();
+        }
+        void Print(string words, bool line = true)
+        {
+            if (!line)
+                System.Console.Write(words);
+            else
+                System.Console.WriteLine(words);
+        }
+    }
+}
