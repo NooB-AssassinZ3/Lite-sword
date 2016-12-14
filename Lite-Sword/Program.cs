c6d1a23ce49104d7838137727e4c65ae66f9887e
NooB-AssassinZ3 <noob.assassinz3orstep@gmail.com>
2016-12-13 19:13:44 -0600

    Reloaded stuff

--- Lite-Sword/Program.cs
+++ Lite-Sword/Program.cs
@@ -8,12 +8,30 @@ namespace Lite_Sword
 {
     class Program
     {
+        bool worked;
         static void Main(string[] args)
         {
-            System.Console.Title = "Lite Sword 0.1.0";
-            Print("1) New Save ");
+            
+
+                
         }
-        static void Print(string words, bool line = true)
+
+        
+
+        bool Menu()
+        {
+            System.Console.Title = "Lite Sword 0.1.0";
+            Print("1) New Save \n2) Load Save \n3) Credits \n4) Exit");
+            int input = System.Console.Read();
+            if (input == 1)
+            {
+                worked = true;
+                //IO.
+            }
+            return worked;
+            
+        }
+        public static void Print(string words, bool line = true)
         {
             if (!line)
                 System.Console.Write(words);
